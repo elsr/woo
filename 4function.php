10d0057ea8dc54e7ac753b5c3534136ec0efbe69
elsr <elsr@list.ru>
2017-07-13 23:18:21 +0500

    увеличение изображения  в товаре

---
+++ 4function.php
@@ -0,0 +1,5 @@
+Как включить инструмент увеличение изображения при наведении мышки в товаре Woocommerce
+
+add_theme_support( 'wc-product-gallery-zoom' );
+add_theme_support( 'wc-product-gallery-lightbox' );
+add_theme_support( 'wc-product-gallery-slider' );
